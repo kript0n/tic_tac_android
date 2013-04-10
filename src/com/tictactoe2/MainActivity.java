5dc4ecf64f8827e0e828415cc2dbe2e806bb053d
doomok2100 <ufhhtn@gmail.com>
2013-04-10 21:10:32 +0900

    Update

--- src/com/tictactoe2/MainActivity.java
+++ src/com/tictactoe2/MainActivity.java
@@ -16,15 +16,14 @@ public class MainActivity extends Activity implements OnClickListener{
 	Drawable picX,picO; 
 	int count=1,b=0;	
 	ImageButton bt1[];
-	boolean newGame = false;//Создавать ли новую игру?
-	boolean EndGame = false;//Конец игры?
-
+	boolean newGame = false;
+	boolean EndGame = false;
 	
 	protected void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
 	    setContentView(R.layout.activity_main);
 	    
-	    bt1 = new ImageButton[] //Задаем массив ImageButton
+	    bt1 = new ImageButton[] 
 	    {			
 	    		(ImageButton)findViewById(R.id.imageButton1),
 				(ImageButton)findViewById(R.id.imageButton2),			
@@ -38,30 +37,27 @@ public class MainActivity extends Activity implements OnClickListener{
 		};
 	    
 	    for(int i = 0; i < 9; i++) {
-	    	bt1[i].setOnClickListener(this); //Для каждой кнопки задаем онклик листенер
+	    	bt1[i].setOnClickListener(this); 
 	    	
-	    	squad[i] = 0; //Обнуляем массив, потому что сначала в нем хранится мусор, и вполне возможно,
-	    	//что какая то из ячеек будет равна 1 или -1, из-за чего будут баги 
+	    	squad[i] = 0; 
 	    }
 	}
 	
-/*-------------------------------Функция рисования------------------------------------*/	
 	
 	private void draw() {
 		for(int i=0;i<9;i++) {
-			if(squad[i] == 1) {//Если в массиве 1, то рисуем крест
+			if(squad[i] == 1) {
 				bt1[i].setImageResource(R.drawable.crest);
 				bt1[i].setClickable(false);
 			}
-			else if(squad[i] == -1) { //Если минус 1, то ноль
+			else if(squad[i] == -1) {
 				bt1[i].setImageResource(R.drawable.zero); 
 				bt1[i].setClickable(false);
 			}
-			if(EndGame) bt1[i].setClickable(false);//Если игра закончена, блокируем все кнопки
+			if(EndGame) bt1[i].setClickable(false);
 		}
 	}
 	
-/*------------------------------------------------------------------------------------*/	
 	
 	@Override
 	public void onClick(View v) {
@@ -70,76 +66,28 @@ public class MainActivity extends Activity implements OnClickListener{
 	    bt=(ImageButton)v;
 		if (count==1) {
 		    count++;	
-		    switch(bt.getId()){
-				case R.id.imageButton1:
-					squad[0] = 1;
-					break;
-				case R.id.imageButton2:
-					squad[1] = 1;
-					break;
-				case R.id.imageButton3:
-					squad[2] = 1;
-					break;
-				case R.id.imageButton4:
-					squad[3] = 1;
-					break;
-				case R.id.imageButton5:
-					squad[4] = 1;
-					break;
-				case R.id.imageButton6:
-					squad[5] = 1;
-					break;
-				case R.id.imageButton7:
-					squad[6] = 1;
-					break;
-				case R.id.imageButton8:
-					squad[7] = 1;
-					break;
-				case R.id.imageButton9:
-					squad[8] = 1;
-					break;
-				default: break;
-			}
+		    for(int i=0; i<9;i++) {
+		    	if(bt.getId() == bt1[i].getId()) {
+		    		squad[i] = 1;
+		    		break;
+		    	}
+		    }
 		}
 		else  {
             count--;
-          	switch(bt.getId()){
-		  		case R.id.imageButton1:
-		  			squad[0] = -1;
-		  			break;
-		  		case R.id.imageButton2:
-		  			squad[1] = -1;
-		  			break;
-		  		case R.id.imageButton3:
-		  			squad[2] = -1;
-		  			break;
-		  		case R.id.imageButton4:
-		  			squad[3] = -1;
-		  			break;
-		  		case R.id.imageButton5:
-		  			squad[4] = -1;
-		  			break;
-		  		case R.id.imageButton6:
-		  			squad[5] = -1;
-		  			break;
-		  		case R.id.imageButton7:
-		  			squad[6] = -1;
-		  			break;
-		  		case R.id.imageButton8:
-		  			squad[7] = -1;
-		  			break;
-		  		case R.id.imageButton9:
-		  			squad[8] = -1;
-		  			break;
-		  			default: break;
+            for(int i=0;i<9;i++) {
+            	if(bt.getId() == bt1[i].getId()) {
+            		squad[i] = -1;
+            		break;
+            	}
             }
 	 
 	}
-	check(); //Проверяем на победителя
-	draw(); //Отрисовываем.
-	/* Сначала проверяем потом отрисовываем, иначе будет возможность после победы заполнить еще одну кнопку*/
+	check(); 
+	draw(); 
+	
 	b++;	
-	if(EndGame) startActReset(); //Если игра закончена, создаем активити с диалогом
+	if(EndGame) startActReset(); 
 
 }
     
@@ -156,7 +104,7 @@ public class MainActivity extends Activity implements OnClickListener{
     		)  
     	{
     		Toast.makeText(this,"Выиграли Крестики" , Toast.LENGTH_LONG).show();
-    		EndGame = true;//Победитель найден, игра закончена
+    		EndGame = true;
     	}
     	
     	else if ( 
@@ -171,56 +119,45 @@ public class MainActivity extends Activity implements OnClickListener{
        		)   
     	{
     		Toast.makeText(this,"Выиграли Нолики" , Toast.LENGTH_LONG).show();
-    		EndGame = true;//Победитель найден, игра закончена
+    		EndGame = true;
     	}
     	
     	else if(b==8) {
-    		EndGame = true;//Если все кнопки заняты, игра закончена
+    		EndGame = true;
     		Toast.makeText(this,"Ничья" , Toast.LENGTH_LONG).show();
     	}
     }
 	
 	
-	
-	//-------------------------Запускаем новую активити с ожиданием результата------//
-	
 	private void startActReset() {
 		Intent resetAct = new Intent(this, reset.class);
 		startActivityForResult(resetAct, 1);		
 	}
+		
 	
-	//------------------------------------------------------------------------------//
-	
-	
-	
-	//------------Как только получили результат вызывается эта функция--------------//
 	
 	@Override 
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		if(data == null) return;
-		newGame = data.getBooleanExtra("newGame", false);//получаем какая кнопка нажата
-		reset(); //вызываем функцию сброса
+		newGame = data.getBooleanExtra("newGame", false);
+		reset(); 
 	}
 	
-	//------------------------------------------------------------------------------//
-	
-	
-	
-	//--------------------------------Функция сброса--------------------------------//
+
 	
 	private void reset() {
-		if(newGame) { //Если была нажата кнопка "Да"
+		if(newGame) { 
 			for(int i=0;i<9;i++) {
-				squad[i] = 0; //Очищаем массив
-				bt1[i].setImageResource(R.drawable.background); //Закрашиваем фоном
-				bt1[i].setClickable(true); //Делаем кликабельными
+				squad[i] = 0; 
+				bt1[i].setImageResource(R.drawable.background);
+				bt1[i].setClickable(true); 
 			}
-			EndGame = false; //Конец игры false
-			b = 0; //Сбрасываем счетчик ничьи
-			draw(); //Отрисовываем
+			EndGame = false; 
+			b = 0; 
+			draw(); 
 		}
-		else finish(); //Если кнопка "Нет", то завершаем работу приложения
+		else finish(); 
 	}
 	
-	//------------------------------------------------------------------------------//
+
 }
